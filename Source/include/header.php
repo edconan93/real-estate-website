fc2b1c75befb05f43d153cdc27a24cefaac90b95
ducthinh100286 <ducthinh100286@gmail.com>
2011-06-22 09:31:55 +0000

    - Thinh

---
+++ Source/include/header.php
@@ -0,0 +1,71 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Real Estate - Hoa Phuong</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link rel="stylesheet" type="text/css" href="../css/default.css">
+	<script type="text/javascript" src="scripts/load.js"></script>
+</head>
+<body style="margin: 0pt; padding: 0pt;" bgcolor="#000c1c">
+	<div style="width: 100%; background-image: url(&quot;../images/bg_top.gif&quot;); background-repeat: repeat-x; text-align: center;">
+		<center>
+            <div style="clear: both; width: 1100px; text-align: center; background-image: url(&quot;../images/logo_top.gif&quot;);
+                background-repeat: no-repeat;">
+                <div style="padding-left: 140px;">
+                    <center>
+                        <div style="height: 60px;">
+                            <table border="0" cellpadding="0" cellspacing="0">
+								<tr>
+									<td>
+										<div id="slogan" style="margin-left: 100px; margin-bottom: 3px;">
+											<embed type="application/x-shockwave-flash" src="../images/slogan.swf" id="mymovie"
+												name="mymovie" bgcolor="#000000" quality="high" loop="true" wmode="transparent"
+												width="530" height="48"></div>
+
+										<script type="text/javascript">
+			var fo = new FlashObject("../images/slogan.swf", "mymovie", "600", "48", "6", "#000000"); 
+			fo.addParam("loop", "true"); 
+			fo.addParam("wmode", "transparent"); 
+			fo.write("slogan"); 
+										</script>
+
+									</td>
+									<td valign="bottom">
+										<div style="margin-bottom: 3px;">
+											<img src="../images/home.png" style="vertical-align: middle;">
+											<a href="dichvu.php" class="a_small">Home</a>&nbsp;&nbsp;&nbsp;
+											<img src="../images/kgpg_key1.png" style="vertical-align: middle;">
+											<a href="http://ankhanhjvc.com/index.php" class="a_small">Đăng Nhập</a>&nbsp;&nbsp;&nbsp;<img
+												src="../images/sign-up.png" style="vertical-align: middle;">
+											<a href="http://ankhanhjvc.com/index.php?setlang=e" class="a_small">Đăng Ký</a></div>
+									</td>
+								</tr>
+                            </table>
+                        </div>
+                        <table border="0" cellpadding="0" cellspacing="0">
+							<tr>
+								<td>
+									<a href="gioithieu.php" onmouseover='bt("t1","../images/menu1_on.gif")'
+										onmouseout='bt("t1","../images/menu1.gif")' onfocus="this.blur();">
+										<img src="../images/menu1.gif" id="t1" style="" border="0"></a>
+								</td>
+								<td>
+									<a href="http://ankhanhjvc.com/index.php?menuid=118" onmouseover='bt("t2","../images/menu/e/menu2_on.gif")'
+										onmouseout='bt("t2","../images/menu2.gif")' onfocus="this.blur();">
+										<img src="../images/menu2.gif" id="t2" style="" border="0"></a>
+								</td>
+								<td>
+									<a href="http://ankhanhjvc.com/index.php?menuid=117" onmouseover='bt("t3","../images/menu/e/menu3_on.gif")'
+										onmouseout='bt("t3","../images/menu3.gif")' onfocus="this.blur();">
+										<img src="../images/menu3.gif" id="t3" style="" border="0"></a>
+								</td>
+								<td>
+									<a href="http://ankhanhjvc.com/index.php?menuid=116" onmouseover='bt("t4","../images/menu4_on.gif")'
+										onmouseout='bt("t4","../images/menu4.gif")' onfocus="this.blur();">
+										<img src="../images/menu4.gif" id="t4" style="" border="0"></a>
+								</td>
+							</tr>
+                        </table>
+                    </center>
+                </div>
+                <center>
---
+++ Source/module/dichvu.php
@@ -0,0 +1,106 @@
+﻿<?php
+	include("../include/header.php");
+?>
+	<table bgcolor="black" border="0" cellpadding="0" cellspacing="0" width="986">
+		<tr>
+			<td width="986">
+				<div style="width: 986px; height: 177px;" id="contentslide">
+					<embed type="application/x-shockwave-flash" src="../images/contentslide.swf" id="mymovie"
+						name="mymovie" bgcolor="#000000" quality="high" loop="true" wmode="transparent"
+						width="986" height="177">
+				</div>
+				<table bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0" width="986;">
+					<tr>
+						<td style="border-right: 1px solid rgb(180, 215, 232); background-repeat: repeat-y;"
+							background="1_files/menubg_all.jpg" valign="top" width="270">
+							<div style="width: 270px; min-height: 500px; background-image: url(&quot;images/menubg_top.jpg&quot;);
+								background-repeat: no-repeat;">
+								<div style="width: 270px; min-height: 500px; background-image: url(&quot;images/sidebg.jpg&quot;);
+									background-repeat: no-repeat; background-position: center bottom;">
+									<div style="height: 20px;">
+									</div>
+									<div class="menulv1">
+										<a class="a_menu" href="http://ankhanhjvc.com/index.php?menuid=119">Introduction</a></div>
+									<div class="menulv2_active">
+										<a class="a_menu2" href="http://ankhanhjvc.com/index.php?menuid=123" style="color: rgb(0, 
+0, 0);">About the Company</a></div>
+									<div class="menulv2">
+										<a class="a_menu2" href="http://ankhanhjvc.com/index.php?menuid=122">Video Clips</a></div>
+								</div>
+							</div>
+							<div style="margin-top: 20px; padding-top: 18px; padding-left: 10px; width: 168px;
+								height: 86px; background-image: url(&quot;images/hitbg.gif&quot;); background-repeat: no-repeat;">
+								<div style="font-family: tahoma; font-size: 12px; font-weight: bold; color: rgb(14, 58, 95);"
+									title="Tính từ 14:15:46 - 21/08/2009">
+									Số lượt truy cập: <span id="hittotal" style="font-family: tahoma; font-size: 12px;
+										color: rgb(14, 58, 95);">74349</span></div>
+								<div style="font-family: tahoma; font-size: 12px; font-weight: bold; color: rgb(14, 58, 95);">
+									Hôm nay: <span id="hittoday" style="font-family: tahoma; font-size: 12px; color: rgb(118, 125, 152);">
+										60112</span></div>
+								<div style="font-family: tahoma; font-size: 11px; font-weight: normal; color: rgb(14, 58, 95);">
+									Có <span id="onlineloc" style="font-family: tahoma; font-size: 12px; color: rgb(14, 58, 95);">
+										5</span> người đang online</div>
+
+							</div>
+							<div style="height: 30px;">
+							</div>
+						</td>
+						<td style="padding: 10px;" valign="top">
+							<div style="width: 686px;">
+								<div style="margin-left: 10px; margin-top: 10px; font-family: tahoma; font-size: 18px;
+									font-weight: bold; color:#890C29;">
+									THÔNG TIN KHÁCH HÀNG</div>
+								<hr style="color: rgb(211, 232, 248);" width="680" size="1">
+								<div style="padding:20px 0;">
+									<table width="100%">
+										<tr>
+											<td>Ông/Bà: <input type="text" value="" /></td>
+											<td align="center">SĐT 1: <input type="text" value="" /></td>
+											<td align="right">SĐT 2: <input type="text" value="" /></td>
+										</tr>
+										<tr>
+											<td>Giá mua:</td>
+											<td align="center">Từ: <input type="text" value="" /></td>
+											<td align="right">Đến: <input type="text" value="" /></td>
+										</tr>
+										<tr>
+											<td>Ngày:</td>
+											<td align="center">Từ: <input type="text" value="" /></td>
+											<td align="right">Đến: <input type="text" value="" /></td>
+										</tr>
+										<tr>
+											<td>Tiêu chí khách hàng:</td>
+											<td colspan="2">
+												<textarea></textarea>
+											</td>
+										</tr>
+										<tr>
+											<td>Các căn nhà đã giới thiệu:</td>
+											<td colspan="2"><textarea></textarea></td>
+										</tr>
+										<tr>
+											<td>Khả năng mua:</td>
+											<td colspan="2">
+												<select>
+													<option>Cao</option>
+													<option>Trung Bình</option>
+													<option>Thấp</option>
+													<option>Đã Mua 1</option>
+													<option>Đã Mua 2</option>
+												</select>
+											</td>
+										</tr>
+									</table>
+									<p style="font-size: 11px; color: #9F9F9F;">
+										(*) Thông tin không được để trống.</p>
+								</div>
+							</div>
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+	</table>
+<?php
+	include("../include/footer.php");
+?>
