fcb28459dba076cde7330eb79d74c85b306371e6
ducthinh100286@gmail.com <ducthinh100286@gmail.com@ed6ee74f-b145-143b-e7de-7fc44ff28af4>
2011-06-22 09:31:55 +0000

    - Thinh

---
+++ trunk/Source/admin/include/footer.php
@@ -0,0 +1,38 @@
+		</center>
+            </div>
+            <div style="width: 100%; height: 40px; background-color: rgb(31, 
+35, 35); border-top: 1px solid rgb(41, 68, 71);">
+                <div style="width: 986px; text-align: left;">
+                    <div style="float: left; margin-top: 8px; font-family: verdana; font-size: 11px;
+                        color: rgb(125, 133, 170); text-align: left;">
+                        Copyright © 2011 realestate_hoaphuong.com
+                    </div>
+                    <div style="float: left; margin-top: 8px; margin-left: 340px; height: 20px; width: 50px;
+                        background-color: rgb(56, 58, 71); border: 1px solid 
+rgb(33, 91, 135); text-align: center;">
+                        <a class="a_bottomlink" href="http://ankhanhjvc.com/index.php?menuid=126">SALES</a></div>
+                    <div style="float: left; margin-top: 8px; margin-left: 10px; height: 20px; width: 50px;
+                        background-color: rgb(56, 58, 71); border: 1px solid 
+rgb(33, 91, 135); text-align: center;">
+                        <a class="a_bottomlink" href="http://ankhanhjvc.com/index.php?menuid=127">Q&amp;A</a></div>
+                    <div style="float: left; margin-top: 8px; margin-left: 10px; height: 20px; width: 50px;
+                        background-color: rgb(56, 58, 71); border: 1px solid 
+rgb(33, 91, 135); text-align: center;">
+                        <a class="a_bottomlink" href="http://www.ankhanhjvc.com:2095/">EMAIL</a></div>
+                    <div style="float: left; margin-top: 8px; margin-left: 10px; height: 20px; padding-left: 5px;
+                        padding-right: 5px; background-color: rgb(56, 
+58, 71); border: 1px solid rgb(33, 91, 135); text-align: center;">
+                        <a class="a_bottomlink" href="http://ankhanhjvc.com/index.php?menuid=150">RECRUITMENTS</a></div>
+                </div>
+            </div>
+            <div style="width: 100%; height: 70px; background-image: url(&quot;images/bg_bottom.gif&quot;);
+                background-repeat: repeat-x; text-align: center; padding-top: 20px;">
+                <a class="a_bottomlink_b" href="http://www.ankhanhjvc.com/">Hoa Phuong</a>&nbsp;<b
+                    style="color: rgb(84, 114, 146);">|</b>&nbsp;<a class="a_bottomlink_b" href="http://diaoc.com/">OC Home Loans & Realty
+                        E&amp;C</a>&nbsp;<b style="color: rgb(104, 135, 168);">|</b>&nbsp;<a class="a_bottomlink_b"
+                            href="http://www.vinaconex.com.vn/">VINACONEX</a>
+            </div>
+        </center>
+	</div>
+</body>
+</html>
---
+++ trunk/Source/admin/module/thongtinkhachhang.php
@@ -0,0 +1,135 @@
+﻿<?php
+	include("../include/header.php");
+?>
+	<script>
+		function checkSDT1()
+		{
+			if (document.getElementById("sdt1").value != "")
+				document.getElementById("sdt2").disabled = false;
+			else
+			{
+				document.getElementById("sdt2").disabled = true;
+				document.getElementById("sdt2").value = "";
+			}
+		}
+	</script>
+	<table bgcolor="black" border="0" cellpadding="0" cellspacing="0" width="986">
+		<tr>
+			<td width="986">
+				<div style="width: 986px; height: 177px;" id="contentslide">
+					<embed type="application/x-shockwave-flash" src="../images/contentslide.swf" id="mymovie"
+						name="mymovie" bgcolor="#000000" quality="high" loop="true" wmode="transparent"
+						width="986" height="177">
+				</div>
+				<table bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0" width="986;">
+					<tr>
+						<td style="border-right: 1px solid rgb(180, 215, 232); background-repeat: repeat-y;"
+							background="1_files/menubg_all.jpg" valign="top" width="270">
+							<div style="width: 270px; min-height: 500px; background-image: url(&quot;images/menubg_top.jpg&quot;);
+								background-repeat: no-repeat;">
+								<div style="width: 270px; min-height: 500px; background-image: url(&quot;images/sidebg.jpg&quot;);
+									background-repeat: no-repeat; background-position: center bottom;">
+									<div style="height: 20px;">
+									</div>
+									<div class="menulv1">
+										<a class="a_menu" href="http://ankhanhjvc.com/index.php?menuid=119">Introduction</a></div>
+									<div class="menulv2_active">
+										<a class="a_menu2" href="http://ankhanhjvc.com/index.php?menuid=123" style="color: rgb(0, 
+0, 0);">About the Company</a></div>
+									<div class="menulv2">
+										<a class="a_menu2" href="http://ankhanhjvc.com/index.php?menuid=122">Video Clips</a></div>
+								</div>
+							</div>
+							<div style="margin-top: 20px; padding-top: 18px; padding-left: 10px; width: 168px;
+								height: 86px; background-image: url(&quot;images/hitbg.gif&quot;); background-repeat: no-repeat;">
+								<div style="font-family: tahoma; font-size: 12px; font-weight: bold; color: rgb(14, 58, 95);"
+									title="Tính từ 14:15:46 - 21/08/2009">
+									Số lượt truy cập: <span id="hittotal" style="font-family: tahoma; font-size: 12px;
+										color: rgb(14, 58, 95);">74349</span></div>
+								<div style="font-family: tahoma; font-size: 12px; font-weight: bold; color: rgb(14, 58, 95);">
+									Hôm nay: <span id="hittoday" style="font-family: tahoma; font-size: 12px; color: rgb(118, 125, 152);">
+										60112</span></div>
+								<div style="font-family: tahoma; font-size: 11px; font-weight: normal; color: rgb(14, 58, 95);">
+									Có <span id="onlineloc" style="font-family: tahoma; font-size: 12px; color: rgb(14, 58, 95);">
+										5</span> người đang online</div>
+
+							</div>
+							<div style="height: 30px;">
+							</div>
+						</td>
+						<td style="padding: 10px;" valign="top">
+							<div style="width: 686px;">
+								<div style="margin-left: 10px; margin-top: 10px; font-family: tahoma; font-size: 18px;
+									font-weight: bold; color:#890C29;">
+									THÔNG TIN KHÁCH HÀNG</div>
+								<hr style="color: rgb(211, 232, 248);" width="680" size="1">
+								<div style="padding:20px 0;">
+									<table width="100%" border="0">
+										<tr>
+											<td>Ông/Bà <span style="color:red;">(*)</span>:
+											<input type="text" value="" /></td>
+											<td align="center">SĐT 1 <span style="color:red;">(*)</span>: <input id="sdt1" type="text" value="" onkeypress="return keypress(event);" onkeyup="checkSDT1();" /></td>
+											<td align="right">SĐT 2: <input id="sdt2" type="text" value="" onkeypress="return keypress(event);" disabled="disabled" /></td>
+										</tr>
+										<tr>
+											<td>Giá mua <span style="color:red;">(*)</span>:</td>
+											<td align="center">Từ: <input type="text" value="" /> Triệu</td>
+											<td align="right">Đến: <input type="text" value="" /> Triệu</td>
+										</tr>
+										<tr>
+											<td>Ngày <span style="color:red;">(*)</span>:</td>
+											<td colspan="2"><script>
+												$(function() {
+													$( "#datepicker" ).datepicker({dateFormat:'dd/mm/yy', showButtonPanel: true});
+												});
+												</script>
+												<input id="datepicker" type="text" style="width:70px;">
+											</td>
+										</tr>
+										<tr>
+											<td>Tiêu chí khách hàng <span style="color:red;">(*)</span>:</td>
+											<td colspan="2">
+												<textarea style="width:100%;"></textarea>
+											</td>
+										</tr>
+										<tr>
+											<td>Tiêu chí khách hàng (Cập nhật):</td>
+											<td colspan="2">
+												<textarea style="width:100%;"></textarea>
+											</td>
+										</tr>
+										<tr>
+											<td>Các căn nhà đã giới thiệu:</td>
+											<td colspan="2"><textarea style="width:100%;"></textarea></td>
+										</tr>
+										<tr>
+											<td>Khả năng mua:</td>
+											<td colspan="2">
+												<select>
+													<option>Cao</option>
+													<option>Trung Bình</option>
+													<option>Thấp</option>
+													<option>Đã Mua 1</option>
+													<option>Đã Mua 2</option>
+												</select>
+											</td>
+										</tr>
+									</table>
+									<div style="padding:20px 0; width:100%; text-align:right;">
+										<img src="../images/btExportToExcel.png" alt="" />
+										<img src="../images/btSave.png" alt="" />
+										<img src="../images/btDelete.png" alt="" />
+									</div>
+									<p style="font-size: 11px; color: #9F9F9F;">
+										 <span style="color:red;">(*)</span> Thông tin không được để trống.</p>
+								</div>
+							</div>
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+	</table>
+<?php
+	include("../include/footer.php");
+?>
